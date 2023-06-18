2345c1454e8625ca4eed0e35f5e476863e51e460
ALOK SINGH <123860933+2004-AlokSINGH@users.noreply.github.com>
2023-06-18 07:55:31 +0530

    Add files via upload
    Assignment

---
+++ callback.js
@@ -0,0 +1,50 @@
+/* 
+Answer1
+
+
+
+def double(number):
+    return number * 2
+
+input_arr = [1, 2, 3, 4, 5]
+doubled_arr = double_elements(input_arr, double)
+print(doubled_arr)
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+function manipulateString(string,logString) {
+    var manipulatedString=string.toUpperCase();
+        return logString
+    function logString() {
+        console.log("The manipulated string is: " + manipulatedString)}
+  }
+  
+  // Example usage:
+var logStringCallback = manipulateString("Hello, World!",logString);
+
+  
+
+// function multiplyByTwo(number) {
+//     return number * 2;
+//   }
+  
+//   function performOperation(operation, number) {
+//     var result = operation(number);
+//     console.log("The result is: " + result);
+//   }
+  
+//   performOperation(multiplyByTwo, 5); // Output: The result is: 10
+  
